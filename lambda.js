8580d50539ec236e8156c9af7bd10444f14b6cc9
jimmy.huang <jimmy.huang@104.com.tw>
2018-11-30 17:51:53 +0800

    update today_message()

--- lambda.js
+++ lambda.js
@@ -20,7 +20,7 @@ const test_options = slack_options('POST', '/api/api.test');
 const post_options = slack_options('POST', '/api/chat.postMessage', post_payload);
 
 function today_message(message_array) {
-    Date.prototype.getDOY = function () {
+    Date.prototype.getDOY = () => {
         var onejan = new Date(this.getFullYear(), 0, 1);
         return Math.ceil((this - onejan) / 86400000);
     };
